7f288e39f731992a5c2a295fc2d1cc0674b6efdf
blackbeans <blackbeans.zc@gmail.com>
2014-11-14 11:22:37 +0800

    修改apns连接数 	modified:   apns/apns_service.go

--- apns/apns_service.go
+++ apns/apns_service.go
@@ -29,7 +29,7 @@ func NewDefaultApnsClient(cert tls.Certificate, pushGateway string,
 	respChan := make(chan *entry.Response, 1000)
 
 	deadline := 10 * time.Second
-	err, factory := NewConnPool(1, 1, 1, 10*time.Second, func(id int32) (error, IConn) {
+	err, factory := NewConnPool(10, 20, 50, 10*time.Second, func(id int32) (error, IConn) {
 		err, apnsconn := NewApnsConnection(respChan, cert, pushGateway, deadline, id)
 		return err, apnsconn
 	})
