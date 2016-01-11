57378425a8b4f5f70f2bc3ebe7d42112cf885936
赵超 <zhao.chao@immomo.com>
2016-01-11 18:30:54 +0800

    add log params 	modified:   ../server/apns_http.go 	modified:   ../server/apns_push_handler.go

--- server/apns_http.go
+++ server/apns_http.go
@@ -125,18 +125,20 @@ func (self *ApnsHttpServer) handlePush(out http.ResponseWriter, req *http.Reques
 		//接卸对应的token和payload
 		token, payload := self.decodePayload(req, resp)
 
+		log := req.PostFormValue("log")
+
 		//----------------如果依然是成功状态则证明当前可以发送
 		if RESP_STATUS_SUCC == resp.Status {
 			defer func() {
 				if re := recover(); nil != re {
-					log.ErrorLog("push_handler", "ApnsHttpServer|handlePush|FAIL|%s", re)
+					log.ErrorLog("push_handler", "ApnsHttpServer|handlePush|FAIL|%s|%s|%s", re, payload, log)
 					resp.Status = RESP_STATUS_ERROR
 					resp.Error = errors.New(fmt.Sprintf("%s", re))
 					self.write(out, resp)
 				}
 			}()
 			self.innerSend(pushType, token, payload, resp)
-			log.InfoLog("push_handler", "ApnsHttpServer|handlePush|%s", payload)
+			log.InfoLog("push_handler", "ApnsHttpServer|handlePush|SUCC|%s|%s", payload, log)
 		}
 
 	}
